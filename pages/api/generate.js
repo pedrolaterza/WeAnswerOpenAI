54e0a296d03b731453772a080bd193504ddfb320
Pedro Laterza <laterza.henrique@gmail.com>
2023-03-06 14:51:31 -0300

    first commit

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -15,21 +15,42 @@ export default async function (req, res) {
     return;
   }
 
-  const animal = req.body.animal || '';
-  if (animal.trim().length === 0) {
+  const nameOrCompany = req.body.nameOrCompany || '';
+  if (nameOrCompany.trim().length === 0){
     res.status(400).json({
       error: {
-        message: "Please enter a valid animal",
+        message: "Please enter a valid name or company",
+      }
+    });
+    return
+  }
+
+  const basicAnswer = req.body.basicAnswer || '';
+  if (basicAnswer.trim().length === 0){
+    res.status(400).json({
+      error: {
+        message: "Please enter a basic anwser",
+      }
+    });
+    return
+  }
+
+  const emailText = req.body.emailText || '';
+  if (emailText.trim().length === 0) {
+    res.status(400).json({
+      error: {
+        message: "Please enter a valid email content",
       }
     });
     return;
-  }
+  } 
 
   try {
     const completion = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt: generatePrompt(animal),
+      prompt: generatePrompt(nameOrCompany,basicAnswer,emailText),
       temperature: 0.6,
+      max_tokens: 2048,
     });
     res.status(200).json({ result: completion.data.choices[0].text });
   } catch(error) {
@@ -48,15 +69,63 @@ export default async function (req, res) {
   }
 }
 
-function generatePrompt(animal) {
-  const capitalizedAnimal =
-    animal[0].toUpperCase() + animal.slice(1).toLowerCase();
-  return `Suggest three names for an animal that is a superhero.
+function generatePrompt(nameOrCompany,basicAnswer,emailText) {
+  let capitalizedName = nameOrCompany;
+  let capitalizedBasicAnswer = basicAnswer;
+  let capitalizedEmailText = emailText;
 
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: ${capitalizedAnimal}
-Names:`;
-}
+  return `Act as an AI-powered Professional Email Generator, Answer this email like a formal human using basic answer information, write a professional formal message in the language you receive the Formalemail.you must be able to understand the context of the email to reply to it.
+
+Name: Pedro Laterza.
+Basic answer: yes we can schedule a call.
+Email: Dear Pedro Laterza,
+
+I hope this email finds you well. I wanted to inquire about your company's services and how they can benefit my business. Please let me know if we can schedule a call to discuss further. Thank you.
+
+Best Regards,
+John Smith.
+
+Formalemail: Dear John Smith,
+
+Thank you for reaching out to me. I am Pedro Laterza, and I appreciate your interest in our company's services.
+
+I would be happy to discuss how our services can benefit your business in more detail. Please let me know your availability for a call, and we can schedule a time that works best for both of us.
+
+Thank you again for considering our company, and I look forward to speaking with you soon
+
+Best regards,
+Pedro Laterza.
+###
+Name: Fernanda Barros.
+Basic answer: thanks for you application i'll be I will be reviewing your resume.
+Email: I am writing to apply for the open position of Marketing Manager at your company. I am excited about the opportunity to join your team and help drive growth for your brand. With 5 years of experience in marketing and a proven track record of success, I believe that I would be an asset to your organization.
+
+In my previous role at XYZ Company, I led a team of marketing professionals to develop and execute successful campaigns across multiple channels. I am highly skilled in digital marketing, including social media, email marketing, and content creation. I am also experienced in event planning and traditional advertising methods. I am confident that my skills and experience make me an ideal candidate for this position.
+
+Thank you for considering my application. I look forward to the opportunity to discuss my qualifications further in an interview.
+
+Sincerely,
+Jonas Ferreira
+
+Formalemail: Dear Jonas Ferreira,
+
+Thank you for your interest in the Marketing Manager position at our company. I am grateful for your application and appreciate the time and effort you put into your submission. I would like to confirm that I have received your resume and will be reviewing it in the coming days.
+
+Your experience in marketing, particularly your success in developing and executing campaigns across multiple channels, caught my attention. Your skills in digital marketing, event planning, and traditional advertising methods align well with the requirements of this role.
+
+We are currently reviewing applications and will be in touch with qualified candidates for further steps in the interview process. If you are selected for an interview, we will contact you to schedule a time and provide additional details about the next steps.
+
+Once again, thank you for your interest in our company and for applying for this position. We appreciate your interest and look forward to reviewing your application.
+
+Best regards,
+Fernanda Barros
+Hiring Manager
+##
+Name: 
+
+Name: """${capitalizedName}."""
+Basic Answer: """${capitalizedBasicAnswer}."""
+Email:"""${capitalizedEmailText}"""
+Formalemail: `
+};
+
--- pages/index.js
+++ pages/index.js
@@ -3,19 +3,25 @@ import { useState } from "react";
 import styles from "./index.module.css";
 
 export default function Home() {
-  const [animalInput, setAnimalInput] = useState("");
+  const [nameInput, setNameInput] = useState("");
+  const [basicAnswerInput, setBasicAnswerInput] = useState("");
+  const [emailTextarea, setEmailTextarea] = useState("");
   const [result, setResult] = useState();
 
   async function onSubmit(event) {
     event.preventDefault();
     try {
-      const response = await fetch("/api/generate", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ animal: animalInput }),
-      });
+        const response = await fetch("/api/generate", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            nameOrCompany: nameInput,
+            basicAnswer: basicAnswerInput,
+            emailText: emailTextarea,
+          }),
+        });
 
       const data = await response.json();
       if (response.status !== 200) {
@@ -23,35 +29,59 @@ export default function Home() {
       }
 
       setResult(data.result);
-      setAnimalInput("");
+      setNameInput("");
+      setBasicAnswerInput("");
+      setEmailTextarea("");
     } catch(error) {
       // Consider implementing your own error handling logic here
       console.error(error);
       alert(error.message);
     }
   }
+  
 
   return (
     <div>
       <Head>
-        <title>OpenAI Quickstart</title>
-        <link rel="icon" href="/dog.png" />
+        <title>We Answer</title>
+        <link rel="icon" href="/mail-check-line.svg" />
       </Head>
 
       <main className={styles.main}>
-        <img src="/dog.png" className={styles.icon} />
-        <h3>Name my pet</h3>
+        <img src="/mail-check-line.svg" className={styles.icon} />
+        <h3>Answer my E-mail or Message</h3>
         <form onSubmit={onSubmit}>
+        <input
+            type="text"
+            name="nameOrCompany"
+            placeholder="Put Your Name or Company"
+            required
+            value={nameInput}
+            onChange={(e) => setNameInput(e.target.value)}
+          />
           <input
             type="text"
-            name="animal"
-            placeholder="Enter an animal"
-            value={animalInput}
-            onChange={(e) => setAnimalInput(e.target.value)}
+            name="basicAnswer"
+            required
+            placeholder="Write a Basic Answer"
+            value={basicAnswerInput}
+            onChange={(e) => setBasicAnswerInput(e.target.value)}
           />
-          <input type="submit" value="Generate names" />
+          <textarea
+            type="text"
+            name="emailText"
+            required
+            placeholder="Email to be Answered"
+            value={emailTextarea}
+            onChange={(e) => setEmailTextarea(e.target.value)}
+          />
+          <input type="submit" value="ANSWER" />
         </form>
         <div className={styles.result}>{result}</div>
+
+        <footer>
+          <p>&copy; 2023 We Answer by <a target="_blank" href="https://www.linkedin.com/in/pedrolaterza/">Pedro Laterza</a>. All rights reserved.</p>
+        </footer>
       </main>
     </div>
   );
